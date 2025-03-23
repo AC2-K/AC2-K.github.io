174eaf3873a89cb4e4c3eaaee001299937400207
AC2-K <barney.y2008@gmail.com>
2025-03-24 01:48:07 +0900

    .

--- src/routes/PostsList.jsx
+++ src/routes/PostsList.jsx
@@ -7,7 +7,7 @@ export default function BlogList() {
         return <div content="BlogsList">
             <h1>記事一覧</h1>
             <div class="row row-cols-1 row-cols-md-2">
-                {
+                {/* {
                     posts.map(
                         (post) => {
                             return <div class="col">
@@ -22,7 +22,7 @@ export default function BlogList() {
                             </div>
                         }
                     )
-                }
+                } */}
             </div>
         </div>;
     } catch {
